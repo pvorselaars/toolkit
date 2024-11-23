3c4e7a012b1eae4128361eb381a73d1ffc1e9495
Perry Vorselaars <p.vorselaars@gmail.com>
2024-11-23 18:34:02 +0100

    Formatting

--- src/ls.c
+++ src/ls.c
@@ -5,80 +5,80 @@
 #include <string.h>
 #include <sys/stat.h>
 
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
 
-  char* path;
-  DIR* dir;
-  struct dirent** list;
-  struct stat status;
-  int error;
+	char *path;
+	DIR *dir;
+	struct dirent **list;
+	struct stat status;
 
-  if (argc > 1){
-    path = argv[1];
-  } else {
-    path = getenv("PWD");
-  }
+	if (argc > 1) {
+		path = argv[1];
+	} else {
+		path = getenv("PWD");
+	}
 
-  error = stat(path, &status);
-  if (error) {
-    fprintf(stderr, "%s: %s: %s\n", argv[0], path, strerror(errno));
-    return 1;
-  }
+	if (stat(path, &status) == -1) {
+		fprintf(stderr, "%s: %s: %s\n", argv[0], path, strerror(errno));
+		return 1;
+	}
 
-  if (S_ISDIR(status.st_mode)) {
+	if (S_ISDIR(status.st_mode)) {
 
-    dir = opendir(path);
-    if (dir) {
+		dir = opendir(path);
+		if (dir) {
 
-      int n = scandir(path, &list, NULL, alphasort);
+			int n = scandir(path, &list, NULL, alphasort);
 
-      if (n == -1) {
-        fprintf(stderr, "%s: %s: %s\n", argv[0], path, strerror(errno));
-        return 2;
-      }
+			if (n == -1) {
+				fprintf(stderr, "%s: %s: %s\n", argv[0], path, strerror(errno));
+				return 2;
+			}
 
-      for (int i = 0; i < n; i++) {
+			for (int i = 0; i < n; i++) {
 
-        char* fullpath = malloc(sizeof(char*) * (strlen(path) + strlen(list[i]->d_name)));
+				char *fullpath = malloc(sizeof(char *) * (strlen(path) + strlen(list[i]->d_name)));
 
-        if (!fullpath) {
-          fprintf(stderr, "%s: %s: %s\n", argv[0], path, strerror(errno));
-          return 3;
-        }
+				if (!fullpath) {
+					fprintf(stderr, "%s: %s: %s\n", argv[0], path, strerror(errno));
+					return 3;
+				}
 
-        sprintf(fullpath, "%s/%s", path, list[i]->d_name);
+				sprintf(fullpath, "%s/%s", path, list[i]->d_name);
 
-        error = stat(fullpath, &status);
-        if (error) {
-          fprintf(stderr, "%s: %s: %s\n", argv[0], fullpath, strerror(errno));
-        }
+				if (stat(fullpath, &status) == -1) {
+					fprintf(stderr, "%s: %s: %s\n", argv[0], fullpath, strerror(errno));
+					return 4;
+				}
 
-        if (S_ISDIR(status.st_mode)) {
-          if (*list[i]->d_name != '.') {
-            printf("%s/\n", list[i]->d_name);
-          }
-        } else {
-          printf("%s\n", list[i]->d_name);
-        }
+				if (S_ISDIR(status.st_mode)) {
+					if (*list[i]->d_name != '.') {
+						printf("%s/\n", list[i]->d_name);
+					}
+				} else {
+					printf("%s\n", list[i]->d_name);
+				}
 
-        free(fullpath);
-        free(list[i]);
+				free(fullpath);
+				free(list[i]);
 
-      }
+			}
 
-      free(list);
+			free(list);
 
-      closedir(dir);
+			closedir(dir);
 
-    } else {
+		} else {
 
-      fprintf(stderr, "%s: %s: %s\n", argv[0], path, strerror(errno));
+			fprintf(stderr, "%s: %s: %s\n", argv[0], path, strerror(errno));
+			return 5;
 
-    }
+		}
 
-  } else {
-    printf("%s\n", path);
-  }
+	} else {
+		printf("%s\n", path);
+	}
 
-  return 0;
+	return 0;
 }
